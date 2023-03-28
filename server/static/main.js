81e864c2f7e8a473c818c4f3955817a04e849377
Tim Schatto-Eckrodt <kudusch@posteo.de>
2023-03-28 23:31:40 +0200

    Add download button

--- server/static/main.js
+++ server/static/main.js
@@ -1,58 +1,47 @@
 "use strict";
 
 var media_attachments = {};
-var $result = $("#result");
+var toots = {};
 $("#file").on("change", function(evt) {
-    // remove content
-    $result.html("");
-    // be sure to show the results
-    $("#result_block").removeClass("hidden").addClass("show");
-
     // Closure to capture the file information.
     function handleFile(f) {
         JSZip.loadAsync(f)
             .then(function(zip) {
-                zip.forEach(function (relativePath, zipEntry) {
+                zip.forEach(function(relativePath, zipEntry) {
                     if (!zipEntry.dir && zipEntry.name.includes("media_attachments") && !zipEntry.name.split('/').reverse()[0].startsWith(".")) {
-                        console.log(zipEntry);
                         media_attachments[zipEntry.name] = zipEntry;
                     }
                     if (zipEntry.name.includes("outbox.json")) {
-                        zipEntry.async("string").then(function (content) {
+                        zipEntry.async("string").then(function(content) {
                             const obj = JSON.parse(content);
                             for (let key in obj["orderedItems"]) {
                                 var e = obj["orderedItems"][key];
                                 if (e["type"] == "Create") {
                                     var toot = {
-                                        "content" : e["object"]["content"],
-                                        "created_at" : e["object"]["published"],
-                                        "url" : e["object"]["url"]
+                                        "id": hashCode(e["object"]["id"]),
+                                        "content": e["object"]["content"],
+                                        "created_at": e["object"]["published"],
+                                        "url": e["object"]["url"]
                                     }
                                     if (typeof e["object"]["updated"] !== "undefined") {
                                         toot["edited_at"] = e["object"]["updated"];
                                     } else {
                                         toot["edited_at"] = "";
                                     }
+                                    //if (toot["id"]) {}
+                                    toots[toot["id"]] = toot;
                                     // if () {
                                     //     e["object"]["attachment"]
                                     // }
-                                    $result.append(
-                                        `
-                                        <article style="border: 1px solid black;">
-                                            <a href='${toot.url}'>${toot.created_at}</a>
-                                            ${toot.content}
-                                            <p>${toot.edited_at}</p>
-                                        </article>
-                                        `);
                                 }
 
                             }
-                            
+
                         });
-                        
+
                     }
                 });
-            }, function (e) {
+            }, function(e) {
                 // $result.append($("<div>", {
                 //     "class" : "alert alert-danger",
                 //     text : "Error reading " + f.name + ": " + e.message
@@ -64,4 +53,52 @@ $("#file").on("change", function(evt) {
     for (var i = 0; i < files.length; i++) {
         handleFile(files[i]);
     }
+    setTimeout(() => {
+        $("#result").html("<p>There are " + Object.keys(toots).length + " toots.</p>");
+        console.log(toots);
+    }, "1000");
+});
+
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function update() {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("var toots = " + JSON.stringify(Object.entries(toots).map((e) => ( e[1] )))) + ";");
+    element.setAttribute('download', "data.js");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function document_ready(callback) {
+    // in case the document is already rendered
+    if (document.readyState != 'loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function() {
+        if (document.readyState == 'complete') callback();
+    });
+}
+
+document_ready(function() {
+    $("#result").html("<p>There are " + Object.keys(toots).length + " toots.</p>");
 });
